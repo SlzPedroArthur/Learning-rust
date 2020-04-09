a5fb289e3facc622047e7fab6a9b552e75212d40
Pedro Arthur <slzpedroarthurcp@protonmail.com>
2020-04-08 23:30:59 -0300

    first commit

---
+++ first-steps/target/debug/deps/hello_cargo-1f9ea0c5533f7781.d
@@ -0,0 +1,5 @@
+/Users/freitas/Documents/Projects/Rust/hello_cargo/target/debug/deps/hello_cargo-1f9ea0c5533f7781.rmeta: src/main.rs
+
+/Users/freitas/Documents/Projects/Rust/hello_cargo/target/debug/deps/hello_cargo-1f9ea0c5533f7781.d: src/main.rs
+
+src/main.rs:
---
+++ first-steps/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/freitas/Documents/Projects/Rust/hello_cargo/target/debug/hello_cargo: /Users/freitas/Documents/Projects/Rust/hello_cargo/src/main.rs
