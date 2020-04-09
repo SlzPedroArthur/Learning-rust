83540af3cc65b896b8abaea49340a5de5483ebad
Pedro Arthur <slzpedroarthurcp@protonmail.com>
2020-04-08 23:54:42 -0300

    random exp.

---
+++ first-steps/src/random.rs
@@ -0,0 +1,11 @@
+extern crate rand;
+
+use rand::Rng;
+
+fn main() {
+   let mut rng = rand::thread_rng();
+
+   let n1: u8 = rng.gen();
+   let n2: u16 rng.gen();
+   println("Random u8: {}", n1)
+}
